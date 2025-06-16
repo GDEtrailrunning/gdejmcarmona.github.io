5b916a0fd216d724772793d0c03c01bd00d6d0c9
GDEtrailrunning <gdeconcienciacorporal@gmail.com>
2025-06-16 10:21:09 -0300

    Commit wekeks 21 & 22

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ document.addEventListener("DOMContentLoaded", function () {
 const dataLine1 = {
   labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4'], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 19',
-    data: [11, 10, 13, 25], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 21',
+    data: [17, 14, 11, 23], // Datos de los km recorridos en cada día
     borderColor: 'rgba(75, 192, 192, 1)',
     fill: true,
     tension: 0.1
@@ -16,8 +16,8 @@ const dataLine1 = {
 const dataLine2 = {
   labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4'], // Etiquetas de los días
   datasets: [{
-    label: 'Total Km recorridos en Semana 20',
-    data: [13, 16, 13, 27],// Datos de los km recorridos en cada día
+    label: 'Total Km recorridos en Semana 22',
+    data: [13, 19, 12, 26],// Datos de los km recorridos en cada día
     borderColor: 'rgba(153, 102, 255, 1)',
     fill: true,
     tension: 0.1
