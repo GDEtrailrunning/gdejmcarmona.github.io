5c203b244c878bc3da3f67a94859e1bace9c582f
GDEtrailrunning <gdeconcienciacorporal@gmail.com>
2025-08-31 13:50:44 -0300

    Commit weeks 31 & 32

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ document.addEventListener("DOMContentLoaded", function () {
 const dataLine1 = {
   labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4'], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 29',
-    data: [13, 14, 15, 20], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 31',
+    data: [15, 15, 13, 30], // Datos de los km recorridos en cada día
     borderColor: 'rgba(75, 192, 192, 1)',
     fill: true,
     tension: 0.1
@@ -16,8 +16,8 @@ const dataLine1 = {
 const dataLine2 = {
   labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4'], // Etiquetas de los días
   datasets: [{
-    label: 'Total Km recorridos en Semana 30',
-    data: [10, 14, 22, 23],// Datos de los km recorridos en cada día
+    label: 'Total Km recorridos en Semana 32',
+    data: [14, 15, 15, 34],// Datos de los km recorridos en cada día
     borderColor: 'rgba(153, 102, 255, 1)',
     fill: true,
     tension: 0.1
@@ -108,15 +108,4 @@ function actualizarDiasRestantes() {
   document.getElementById("Semana 2").appendChild(video3);
   document.getElementById("Semana 2").appendChild(video4);
 
-  audios.forEach(audio => {
-    audio.addEventListener("play", () => {
-      audios.forEach(otherAudio => {
-        if (otherAudio !== audio) {
-          otherAudio.pause();
-        }
-      });
-    });
-  });
-
 });
-
