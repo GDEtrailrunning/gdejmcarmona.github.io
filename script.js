0fd0ed9faa83959a01169e6e8910bdf8ebbe56c5
GDEtrailrunning <gdeconcienciacorporal@gmail.com>
2026-06-29 01:30:18 -0300

    Subo semana 25 y 26

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ document.addEventListener("DOMContentLoaded", function () {
 const dataLine1 = {
   labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4'], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 23',
-    data: [12, 14, 12, 26], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 25',
+    data: [15, 16, 20, 25], // Datos de los km recorridos en cada día
     borderColor: 'rgba(75, 192, 192, 1)',
     fill: true,
     tension: 0.1
@@ -16,8 +16,8 @@ const dataLine1 = {
 const dataLine2 = {
   labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4'], // Etiquetas de los días
   datasets: [{
-    label: 'Total Km recorridos en Semana 24',
-    data: [10, 9, 15, 19],// Datos de los km recorridos en cada día
+    label: 'Total Km recorridos en Semana 26',
+    data: [14, 10, 13, 21],// Datos de los km recorridos en cada día
     borderColor: 'rgba(45, 78, 199, 1)',
     fill: true,
     tension: 0.1
